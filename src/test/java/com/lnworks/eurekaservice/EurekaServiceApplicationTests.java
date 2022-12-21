c9bd1a3692e7a0b955c5bc85be27bc4de8c6872d
LeeKyoungSoo <ilovebasic@gmail.com>
2022-12-21 21:40:30 +0900

    Initial commit

---
+++ src/test/java/com/lnworks/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lnworks.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
